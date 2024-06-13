172d3ff6951a2850a8e88e0a5c685c79f2e2ad6f
Cole Shegan Siniawski <shegan_c1@denison.edu>
2024-06-13 16:33:07 -0400

    Adding team colors to graphs and fixing some code errors

--- Siniawski/2023_WNBA_Playoffs/app.R
+++ Siniawski/2023_WNBA_Playoffs/app.R
@@ -11,20 +11,7 @@ library(shiny)
 library(tidyverse)
 library(wehoop)
 
-pbp <- load_wnba_pbp(2023)
-pbp <- wnba_pbp |> 
-  filter(shooting_play) |> 
-  # make a column to indicate the shooting team
-  mutate(shooting_team = ifelse(team_id == home_team_id, 
-                                home_team_name,
-                                away_team_name)) |> 
-  select(game_id, game_play_number, game_type = season_type,
-         desc = text, shot_type = type_text, 
-         made_shot = scoring_play, shot_value = score_value, 
-         coordinate_x, coordinate_y, shooting_team, 
-         home_team_name, away_team_name, home_score, away_score, qtr,
-         quarter_seconds_remaining = start_quarter_seconds_remaining,
-         game_seconds_remaining = start_game_seconds_remaining)
+
 
 
 # Define UI for application that draws a histogram
@@ -32,7 +19,7 @@ ui <- fluidPage(
     h1("2023 WNBA Playoff Data"),
     selectInput(inputId = "sel_team",
                 label = "Choose a Team:",
-                list(shooting_team)),
+                choices = c("")),
     plotOutput("distPlot")
     )
 
@@ -41,19 +28,37 @@ server <- function(input, output) {
 
 
     output$distPlot <- renderPlot({
-      pbp_shots <- pbp
       
-        df <- pbp_shots |> filter(shooting_team %in% input$sel_team & game_type == 3) |> 
+      pbp <- load_wnba_pbp(2023)
+      pbp <- wnba_pbp |> 
+        filter(shooting_play) |> 
+        # make a column to indicate the shooting team
+        mutate(shooting_team = ifelse(team_id == home_team_id, 
+                                      home_team_name,
+                                      away_team_name)) |> 
+        select(game_id, game_play_number, game_type = season_type,
+               desc = text, shot_type = type_text, 
+               made_shot = scoring_play, shot_value = score_value, 
+               coordinate_x, coordinate_y, shooting_team, 
+               home_team_name, away_team_name, home_score, away_score, qtr,
+               quarter_seconds_remaining = start_quarter_seconds_remaining,
+               game_seconds_remaining = start_game_seconds_remaining)
+      
+      df <- pbp |> filter(shooting_team %in% input$sel_team & game_type == 3) |> 
           group_by(shooting_team, time_group) |> summarise(num_shots = n(), total_shots_made = sum(made_shot)) |> 
           mutate(prop = round(total_shots_made / num_shots, 3))
       
-        g <- ggplot(df, aes(x = time_group, y = prop))
-        g + geom_line(alpha = 0.4) + geom_smooth(se = FALSE)
+      g <- ggplot(df, aes(x = time_group, y = prop))
+      g + geom_line(alpha = 0.4) + geom_smooth(se = FALSE)
     })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
 
 
 
